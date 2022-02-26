45dfedab80a9207baf50c89dfd6beaf90055c9f9
chak <chakcheung4565@outlook.com>
2022-02-26 17:33:22 +0800

    【配置】优化输出和配置

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import routes from './routes'
+import logger from './utils/logger'
+import config from 'config'
 
 const app = express()
 
 app.use(express.json())
 
-const POST = 1337
+const PORT = config.get<number>('port')
 
-app.listen(POST, async () => {
-  console.log(`App is running at http://localhost:${POST}`)
+app.listen(PORT, async () => {
+  logger.info(`App is running at http://localhost:${PORT}`)
   routes(app)
 })
